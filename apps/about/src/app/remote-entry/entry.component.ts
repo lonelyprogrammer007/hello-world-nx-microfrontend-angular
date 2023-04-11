e47c66ca9728390d1c48a2cc5a73cf6b66945b7f
lugfigue <lugfigue@bancolombia.com.co>
2023-04-11 14:50:50 -0500

    host with 3 remotes example generation

---
+++ apps/about/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "hello-world-nx-about-entry",
+  template: `<hello-world-nx-nx-welcome></hello-world-nx-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
